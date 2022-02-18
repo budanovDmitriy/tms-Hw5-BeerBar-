e1f44ecd09c629dabe7fb0ce2029c78f50ae062b
budanovDmitriy <lllytnick1@gmail.com>
2022-02-18 16:21:02 +0300

    commit

---
+++ Hw5/BarManager.swift
@@ -0,0 +1,51 @@
+//
+//  BarManager.swift
+//  Hw5
+//
+//  Created by Dmitriy Budanov on 16.02.2022.
+//
+
+import UIKit
+
+class BarManager {
+
+    public var beers: [Beer] = [Beer(price: 80, name: "IPA", country: "BLR",overalVolume: 200), Beer(price: 90, name: "APA",country: "RUS",overalVolume: 250),Beer(price: 100, name: "OPA",country: "GMN",overalVolume: 280)]
+    private var money: Int
+    static public var shared: BarManager = BarManager()
+    private init() { self.money = 0 }
+    public func buyBeer(name: String, volume: Int) -> Int {
+        guard let buyingBeer = beers.filter({ $0.name == name }).first else { return 0 }
+        let price = buyingBeer.price * volume
+        beers += beers.filter({ $0.name == name }).map { (p) -> Beer in
+            return  Beer(price: p.price, name: p.name, country: p.country, overalVolume: p.overalVolume - volume)
+    
+        }
+        money += price
+        return money
+    }
+    
+    func countMoney() -> Int {
+        return self.money
+    }
+    
+    func calculateVolume (name:String) -> Int {
+        return beers.filter { element in
+            element.name == name
+        }.sorted { left,right in
+            left.overalVolume < right.overalVolume
+        }.first!.overalVolume
+    }
+    
+    func startNewDay() {
+        money = 0
+    }
+
+}
+
+
+struct Beer {
+    let price: Int
+    let name: String
+    let country: String
+    var overalVolume: Int
+}
---
+++ Hw5/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Hw5
+//
+//  Created by Dmitriy Budanov on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var setter: Int = 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLabels()
+        
+    
+        // Do any additional setup after loading the view.
+        //BarManager.shared.calculateVolume(name: "APA")
+        
+    }
+   
+    @IBAction func stepper(_ sender: UIStepper) {
+        label.text = "Volume Of Beer: " + String(sender.value)
+        setter = Int(sender.value)
+    }
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func BuyingFirstBeer(_ sender: Any) {
+        BarManager.shared.buyBeer(name: "IPA", volume: setter)
+        BuyingStatus.text = "You buy \(setter) liters of IPA"
+    }
+    @IBAction func BuyingSecondBeer(_ sender: Any) {
+        BarManager.shared.buyBeer(name: "APA", volume: setter)
+        BuyingStatus.text = "You buy \(setter) liters of APA"
+    }
+    @IBAction func BuyingThirdBeer(_ sender: Any) {
+        BarManager.shared.buyBeer(name: "OPA", volume: setter)
+        BuyingStatus.text = "You buy \(setter) liters of OPA"
+    }
+    @IBOutlet weak var FirstBeerInfo: UILabel!
+    @IBOutlet weak var SecondBeerInfo: UILabel!
+    @IBOutlet weak var ThirdBeerInfo: UILabel!
+    @IBOutlet weak var Earning: UILabel!
+    
+    @IBAction func GetEarnings(_ sender: Any) {
+        Earning.text = "Earning: " + String(BarManager.shared.countMoney())
+    }
+    @IBAction func SetNewDay(_ sender: Any) {
+        BarManager.shared.startNewDay()
+        Earning.text = "Earning: " + String(BarManager.shared.countMoney())
+        
+        
+    }
+    @IBOutlet weak var BuyingStatus: UILabel!
+    @IBOutlet weak var CurrentVolumeFirstBeer: UILabel!
+    @IBAction func GetCurrentVolume(_ sender: Any) {
+        CurrentVolumeFirstBeer.text = "IPA: " + String(BarManager.shared.calculateVolume(name: "IPA"))
+        CurrentVolumeSecondBeer.text = "IPA: " + String(BarManager.shared.calculateVolume(name: "APA"))
+        CurrentVolumeThirdBeer.text = "OPA: " + String(BarManager.shared.calculateVolume(name: "OPA"))
+    }
+    
+    @IBOutlet weak var CurrentVolumeSecondBeer: UILabel!
+    
+    @IBOutlet weak var CurrentVolumeThirdBeer: UILabel!
+    func setupLabels(){
+        FirstBeerInfo.text = BarManager.shared.beers[0].name + " " + BarManager.shared.beers[0].country + " " +
+            String(BarManager.shared.beers[0].price)
+        SecondBeerInfo.text = BarManager.shared.beers[1].name + " " + BarManager.shared.beers[1].country + " " +
+            String(BarManager.shared.beers[1].price)
+        ThirdBeerInfo.text = BarManager.shared.beers[2].name + " " + BarManager.shared.beers[2].country + " " +
+            String(BarManager.shared.beers[2].price)
+        label.text = "Volume Of Beer: 1.0"
+        Earning.text = "Earning: 0"
+        BuyingStatus.text = "Buy some beer"
+    }
+}
